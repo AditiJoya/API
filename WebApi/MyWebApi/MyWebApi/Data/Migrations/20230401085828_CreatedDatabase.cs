21d2cde7f0a425d864f2a060cbe41e0c45fee175
Your Name <you@example.com>
2023-10-17 13:19:26 +0600

    First Commit

---
+++ WebApi/MyWebApi/MyWebApi/Data/Migrations/20230401085828_CreatedDatabase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWebApi.Data.Migrations
+{
+    public partial class CreatedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<int>(type: "int", nullable: false),
+                    Phone = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ WebApi/MyWebApi/MyWebApi/Model/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace MyWebApi.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Address { get; set; }
+        public int Phone { get; set; }
+        public int Email { get; set; }
+    }
+}
